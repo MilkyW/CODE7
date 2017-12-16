cd665535276ab2f23d9aaabd699d74baf96c7798
Atorios <875241499@qq.com>
2017-12-17 01:44:27 +0800

    basic inputform

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,15 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+// require('styles/App.css');
 
 import React from 'react';
-
-let yeomanImage = require('../images/yeoman.png');
+import InputForm from './InputFormComponent'
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+        <div>
+          <InputForm />
+        </div>
     );
   }
 }
