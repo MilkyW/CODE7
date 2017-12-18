b995fd2353c61e4ae70b80cc9fd5cc4afd90c874
kaclarpt <kaclarpt@126.com>
2017-12-18 18:06:37 +0800

    init

---
+++ project2/app/components/release.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import{Navbar,Button}from "react-bootstrap"
+export default React.createClass({
+  render(){
+    return <div>
+      This is the release page.
+    </div>
+  }
+})
