d2ee6c50254a3881c407414238382e7d0af68eba
DESKTOP-F109P7R\loluz <zy409815681@qq.com>
2017-12-19 13:01:50 +0800

    no message

--- personal-center/src/components/Main.js
+++ personal-center/src/components/Main.js
@@ -3,13 +3,38 @@ require('styles/App.css');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+var courseArray = [];
+var collectionArray = [];
+
+class Singlecourse extends React.Component {
+  handleModify(e) {
+
+  }
+  render() {
+    return (
+      <div>
+        <a href={this.props.link}> {this.props.title} </a>
+        <button onClick={this.handleModify.bind(this)}> 修改 </button>
+      </div>
+    );
+  }
+}
+
+class Singlecollection extends React.Component {
+  render() {
+    return (
+      <div>
+        <a href={this.props.link}> {this.props.title} </a>
+      </div>
+    );
+  }
+}
 
 class Interaction extends React.Component {
   render() {
     return (
       <div className="interaction">
-        <span>互动消息</span>
+        <h2>互动消息</h2>
       </div>
     );
   }
@@ -17,9 +42,16 @@ class Interaction extends React.Component {
 
 class Mycourse extends React.Component {
   render() {
+    var courselist = [];
+
+    courseArray.forEach(function(value){
+      courselist.push(<Singlecourse link={value["link"]} title={value["title"]} />);
+    }.bind(this));
+
     return (
-      <div className="mycourse">
-        <span>我的教程</span>
+      <div className="interaction">
+        <h2>我的教程</h2>
+        <div>{courselist}</div>
       </div>
     );
   }
@@ -27,9 +59,16 @@ class Mycourse extends React.Component {
 
 class Mycollection extends React.Component {
   render() {
+    var collectionlist = [];
+
+    courseArray.forEach(function(value){
+      collectionlist.push(<Singlecollection link={value["link"]} title={value["title"]} />);
+    }.bind(this));
+
     return (
-      <div className="mycollection">
-        <span>我的收藏</span>
+      <div className="interaction">
+        <h2>我的收藏</h2>
+        <div>{collectionlist}</div>
       </div>
     );
   }
